0251ab0d4e590f5e92430a15352ce0831b3ee3d8
RynGhd <ryan.ghadiri@gmail.com>
2023-01-12 17:22:41 +1100

    added binary range search

---
+++ Algorithms.UnitTests/BinaryRangeSearchSolutionSolution_Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+// ReSharper disable All
+
+namespace Algorithms.UnitTests;
+
+public class BinaryRangeSearchSolutionSolution_Tests
+{
+    [TestCase(new int[] { 3, 4 }, new int[] { 5, 7, 7, 8, 8, 10 }, 8)]
+    [TestCase(new int[] { 3, 6 }, new int[] { 5, 7, 7, 8,8,8, 8, 10 }, 8)]
+    [TestCase(new int[] { 0,0 }, new int[] { 1 }, 1)]
+
+    public void Tests(int[] output, int[] input, int target)
+    {
+        var sut = new BinaryRangeSearchSolution();
+
+        var result = sut.SearchRange(input, target);
+
+        CollectionAssert.AreEqual(output, result);
+    }
+}
---
+++ Algorithms/BinaryRangeSearchSolution.cs
@@ -0,0 +1,55 @@
+﻿namespace Algorithms;
+
+/// <summary>
+/// question:
+/// solution: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/1136731/find-first-and-last-position-of-element-in-sorted-array/?envType=study-plan&id=algorithm-ii
+/// 
+/// </summary>
+public class BinaryRangeSearchSolution
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        if (nums == null || nums.Length == 0)
+            return new int[2] { -1, -1 };
+
+        var lower = BinarySearch(nums, target, 0, true);
+        if (lower < 0)
+            return new int[2] { -1, -1 };
+
+        var upper = BinarySearch(nums, target, lower, false);
+
+        return new int[2] { lower, upper };
+    }
+
+    private int BinarySearch(int[] nums, int target, int lowerBound, bool isFindingLowerBound)
+    {
+        int low = lowerBound, high = nums.Length - 1;
+
+        while (low <= high)
+        {
+            var mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+            {
+                if (isFindingLowerBound) // search lower bound
+                {
+                    if (mid == low || nums[mid - 1] != target)
+                        return mid;
+
+                    high = mid - 1;
+                }
+                else // search upper bound
+                {
+                    if (mid == high || nums[mid + 1] != target)
+                        return mid;
+
+                    low = mid + 1;
+                }
+            }
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return -1;
+    }
+}
