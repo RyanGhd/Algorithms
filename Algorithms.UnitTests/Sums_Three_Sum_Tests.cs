20fdafa7c90996d63d9e5b55212b31f39ffc2f48
RynGhd <ryan.ghadiri@gmail.com>
2023-01-16 14:43:02 +1100

    added 3 sum

---
+++ Algorithms.UnitTests/Sums_Three_Sum_Tests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Api;
+
+// ReSharper disable All
+
+namespace Algorithms.UnitTests;
+
+public class Sums_Three_Sum_Tests
+{
+    [TestCase(new int[] { -1, 0, 1, 2, -1, -4 }, 0)]
+    [TestCase(new int[] { 0, 1, 1 }, 1)]
+    [TestCase(new int[] { 0, 0, 0 }, 2)]
+    [TestCase(new int[] { 0, 0, 0, 0 }, 3)]
+    public void Tests(int[] input, int outputIndex)
+    {
+        var sut = new Sums_Three_Sum();
+
+        var result = sut.ThreeSum(input);
+
+        var output = TestData.Data[outputIndex];
+
+        if (output.Count == 0) Assert.That(result.Count == 0);
+        else
+        {
+            Assert.That(output.Count == result.Count);
+
+            var matched = false;
+            for (int i = 0; i < output.Count; i++)
+            {
+                for (int j = 0; j < result.Count; j++)
+                {
+                    if (output[i].Except(result[j]).Count() == 0 &&
+                        result[j].Except(output[i]).Count() == 0)
+                    {
+                        matched = true;
+                        break;
+                    }
+                }
+
+            }
+
+            Assert.IsTrue(matched);
+        }
+
+    }
+
+    public static class TestData
+    {
+        public static List<List<List<int>>> Data { get; }
+
+        static TestData()
+        {
+            Data = new List<List<List<int>>>();
+            //scenario 0 
+            Data.Add(new List<List<int>> { new List<int>() { -1, -1, 2 }, new List<int>() { -1, 0, 1 } });
+
+            //scenario 1
+            Data.Add(new List<List<int>> { });
+
+            //scenario 2
+            Data.Add(new List<List<int>> { new List<int>() { 0, 0, 0 } });
+
+            //scenario 3
+            Data.Add(new List<List<int>> { new List<int>() { 0, 0, 0 } });
+            
+        }
+    }
+}
---
+++ Algorithms/Sums_Three_Sum.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace Algorithms;
+
+/// <summary>
+/// question: https://leetcode.com/problems/3sum/solutions/?envType=study-plan&id=algorithm-ii
+/// solution: https://leetcode.com/problems/3sum/solutions/593246/3sum/?envType=study-plan&id=algorithm-ii
+/// PDF added for the solution
+/// </summary>
+public class Sums_Three_Sum
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var numbers = nums.OrderBy(i => i).ToArray();
+
+        if (numbers == null || numbers.Length <= 2) return new List<IList<int>>();
+
+        var result = new List<IList<int>>() { };
+
+        for (int i = 0; i < numbers.Length && (i == 0 || numbers[i] != numbers[i - 1]); i++) // (i == 0 || numbers[i] != numbers[i - 1]): removes duplicates 
+        {
+            TwoSum(numbers, i, result);
+        }
+
+        return result;
+    }
+
+    private void TwoSum(int[] nums, int i, IList<IList<int>> output)
+    {
+        var target = nums[i];
+
+        var seen = new HashSet<int>();
+
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            var remaining = ((-1) * target) - nums[j];
+
+            if (seen.Contains(remaining))
+            {
+                output.Add(new List<int> { target, remaining, nums[j] });
+
+                // to avoid duplicates in second iteration
+                // this should happen only if we add the item to the list
+                // otherwise it fails for nums:[0,0,0]
+                while (j < nums.Length - 1 && nums[j] == nums[j + 1])
+                {
+                    j++;
+                }
+            }
+
+            if (!seen.Contains(nums[j]))
+                seen.Add(nums[j]);
+        }
+    }
+
+}
