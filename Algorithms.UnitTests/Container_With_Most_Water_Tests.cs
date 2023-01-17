898476269c865a5c1dc18da57c46af4f7b521835
RynGhd <ryan.ghadiri@gmail.com>
2023-01-17 18:53:17 +1100

    added the container with most water

---
+++ Algorithms.UnitTests/Container_With_Most_Water_Tests.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable All
+
+using NUnit.Framework;
+
+namespace Algorithms.UnitTests;
+
+public class Container_With_Most_Water_Tests
+{
+    [TestCase(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+    public void Tests(int[] input, int output)
+    {
+        var sut = new Container_With_Most_Water();
+
+        var result = sut.MaxArea(input);
+
+        Assert.AreEqual(output, result);
+    }
+}
---
+++ Algorithms/Container_With_Most_Water.cs
@@ -0,0 +1,38 @@
+﻿// ReSharper disable All
+namespace Algorithms;
+
+/// <summary>
+/// question: https://leetcode.com/problems/container-with-most-water/submissions/879750493/?envType=study-plan&id=algorithm-ii
+/// solution: https://leetcode.com/problems/container-with-most-water/solutions/127443/container-with-most-water/
+/// Time complexity: O(n)O(n)O(n). Single pass.
+/// Space complexity: O(1)O(1)O(1). Constant space is used.
+/// </summary>
+public class Container_With_Most_Water
+{
+    public int MaxArea(int[] height)
+    {
+        if (height == null || height.Length == 0) return 0;
+
+        int max = 0,left = 0,right = height.Length-1;
+
+        while (left < right)
+        {
+            var area = CalcArea(left, height[left], right, height[right]);
+            
+            if (area > max) max = area;
+
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+
+        return max;
+    }
+
+    private int CalcArea(int x1,int y1,int x2, int y2)
+    {
+        var x = x2 - x1;
+        var y = Math.Min(y1, y2);
+
+        return x * y;
+    }
+}
