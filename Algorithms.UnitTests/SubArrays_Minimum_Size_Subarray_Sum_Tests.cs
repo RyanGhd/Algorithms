fd648813b5ef92368b84a37e8d6146fea71146d4
RynGhd <ryan.ghadiri@gmail.com>
2023-01-20 14:16:30 +1100

    added SubArrays_Minimum_Size_Subarray_Sum

---
+++ Algorithms.UnitTests/SubArrays_Minimum_Size_Subarray_Sum_Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+// ReSharper disable All
+
+namespace Algorithms.UnitTests;
+
+public class SubArrays_Minimum_Size_Subarray_Sum_Tests
+{
+    [TestCase(7,new int[]{ 2, 3, 1, 2, 4, 3 },2)]
+    [TestCase(4,new int[]{ 1, 4, 4 },1)]
+    [TestCase(11,new int[]{ 1, 1, 1, 1, 1, 1, 1, 1 },0)]
+    public void Tests(int target,int[] nums,int output)
+    {
+        var sut = new SubArrays_Minimum_Size_Subarray_Sum();
+
+        var result = sut.MinSubArrayLen(target, nums);
+
+        Assert.AreEqual(output, result);
+    }
+}
---
+++ Algorithms/SubArrays_Minimum_Size_Subarray_Sum.cs
@@ -0,0 +1,47 @@
+﻿// ReSharper disable All
+namespace Algorithms;
+
+/// <summary>
+/// question: https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan&id=algorithm-ii
+/// solution: sliding window: https://leetcode.com/problems/minimum-size-subarray-sum/solutions/127732/minimum-size-subarray-sum/
+///
+/// Time complexity: O(n)
+/// Space complexity: O(1)
+/// </summary>
+public class SubArrays_Minimum_Size_Subarray_Sum
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        if (nums == null || nums.Length == 0) return 0;
+
+        int total = 0;
+        int min = nums.Length + 1;
+
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] >= target)
+                return 1;
+
+            total += nums[right];
+
+            if (total < target)
+                continue;
+
+            while (total >= target && left < right)
+            {
+                if (min > right - left + 1)
+                    min = right - left + 1;
+
+                total -= nums[left];
+                left++;
+            }
+        }
+
+        if (min > nums.Length)
+            return 0;
+
+        return min;
+    }
+}
